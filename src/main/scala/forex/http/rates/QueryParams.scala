951112e4cd70d1818e3b60a448792d23acc97db1
deshuai-hoho <ywthoho@qq.com>
2023-12-26 00:40:13 +0800

    add validation logic of query parameters

--- src/main/scala/forex/http/rates/QueryParams.scala
+++ src/main/scala/forex/http/rates/QueryParams.scala
@@ -3,11 +3,14 @@ package forex.http.rates
 import forex.domain.Currency
 import org.http4s.QueryParamDecoder
 import org.http4s.dsl.impl.QueryParamDecoderMatcher
+import org.http4s.ParseFailure
 
 object QueryParams {
 
   private[http] implicit val currencyQueryParam: QueryParamDecoder[Currency] =
-    QueryParamDecoder[String].map(Currency.fromString)
+    QueryParamDecoder[String].emap{ str =>
+      Currency.fromString(str).toRight(ParseFailure("Invalid currency", s"$str is not a valid currency"))  
+    }
 
   object FromQueryParam extends QueryParamDecoderMatcher[Currency]("from")
   object ToQueryParam extends QueryParamDecoderMatcher[Currency]("to")
--- src/main/scala/forex/http/rates/RatesHttpRoutes.scala
+++ src/main/scala/forex/http/rates/RatesHttpRoutes.scala
@@ -2,24 +2,48 @@ package forex.http
 package rates
 
 import cats.effect.Sync
-import cats.syntax.flatMap._
+import cats.implicits._
 import forex.programs.RatesProgram
 import forex.programs.rates.{ Protocol => RatesProgramProtocol }
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
+import org.http4s.headers.Allow
+import org.http4s.Method
+import forex.domain.Currency
 
 class RatesHttpRoutes[F[_]: Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
 
-  import Converters._, QueryParams._, Protocol._
+  import Converters._, Protocol._
 
   private[http] val prefixPath = "/rates"
 
+  object OptionalFromQueryParam extends OptionalQueryParamDecoderMatcher[String]("from")
+  object OptionalToQueryParam extends OptionalQueryParamDecoderMatcher[String]("to")
+
   private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
-    case GET -> Root :? FromQueryParam(from) +& ToQueryParam(to) =>
-      rates.get(RatesProgramProtocol.GetRatesRequest(from, to)).flatMap(Sync[F].fromEither).flatMap { rate =>
-        Ok(rate.asGetApiResponse)
+
+    // Validate query parameters
+    case GET -> Root :? OptionalFromQueryParam(fromOpt) +& OptionalToQueryParam(toOpt) =>
+      (fromOpt, toOpt) match {
+        case (Some(fromStr), Some(toStr)) =>
+          (Currency.fromString(fromStr), Currency.fromString(toStr)) match {
+            case (Some(from), Some(to)) =>
+              rates.get(RatesProgramProtocol.GetRatesRequest(from, to))
+                .flatMap(Sync[F].fromEither)
+                .flatMap( rate => Ok(rate.asGetApiResponse))
+                .handleErrorWith {
+                  case _ => InternalServerError("Internal server error")
+              }
+            case (None, None) => BadRequest("Both 'from' and 'to' query parameters are invalid currencies.")
+            case (None, _) => BadRequest("'from' query parameter is an invalid currency.")
+            case (_, None) => BadRequest("'to' query parameter is an invalid currency.")
+          }
+        case (None, None) => BadRequest("Missing both 'from' and 'to' query parameters.")
+        case (None, _) => BadRequest("Missing 'from' query parameter.")
+        case (_, None) => BadRequest("Missing 'to' query parameter.")
       }
+    case _ => MethodNotAllowed(Allow(Method.GET))
   }
 
   val routes: HttpRoutes[F] = Router(
