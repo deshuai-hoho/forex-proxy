5265a3a115f7413c3296e38736a00b1bbb8ab788
deshuai-hoho <ywthoho@qq.com>
2023-12-28 20:32:30 +0800

    chores: remove gitignore files

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,6 +14,7 @@ object Dependencies {
     val logback        = "1.2.3"
     val scalaCheck     = "1.15.3"
     val scalaTest      = "3.2.7"
+    val scalaMock      = "5.1.0"
     val catsScalaCheck = "0.3.2"
   }
 
@@ -27,6 +28,7 @@ object Dependencies {
 
     lazy val http4sDsl       = http4s("http4s-dsl")
     lazy val http4sServer    = http4s("http4s-blaze-server")
+    lazy val http4sClient    = http4s("http4s-blaze-client")
     lazy val http4sCirce     = http4s("http4s-circe")
     lazy val circeCore       = circe("circe-core")
     lazy val circeGeneric    = circe("circe-generic")
@@ -43,6 +45,7 @@ object Dependencies {
     // Test
     lazy val scalaTest      = "org.scalatest"     %% "scalatest"       % Versions.scalaTest
     lazy val scalaCheck     = "org.scalacheck"    %% "scalacheck"      % Versions.scalaCheck
+    lazy val scalaMock      = "org.scalamock"     %% "scalamock"       % Versions.scalaMock
     lazy val catsScalaCheck = "io.chrisdavenport" %% "cats-scalacheck" % Versions.catsScalaCheck
   }
 
